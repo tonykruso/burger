5112e35c89196eef94ed24513daa8ef29edbe8ed
Tony Kruso <tony.kruso@gmail.com>
2019-05-30 13:49:05 -0400

    added delete function

--- models/burger.js
+++ models/burger.js
@@ -20,6 +20,12 @@ var burger = {
         orm.update('burgers', objColVals, condition, function(res){
             cb(res);
         });
+    },
+
+    delete: function(condition, cb){
+        orm.delete('burgers', condtion, function(res){
+            cb(res);
+        })
     }
 };
 
